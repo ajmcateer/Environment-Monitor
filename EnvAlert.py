477a94761d1a2b5d735097f7a582a624cc21807e
Andrew John McAteer <ajmcateer@gmail.com>
2017-02-27 01:49:23 -0500

    Initial Commit

---
+++ EnvAlert.py
@@ -0,0 +1,35 @@
+"""Checks the enviroment and sends an email to affected user"""
+import os
+import requests
+
+def send_alert(alert):
+    """Send notification"""
+    print(alert)
+
+def https_check(website):
+    """Checks http resources"""
+    try:
+        result = requests.get(website, verify=False)
+    except:
+        return "404"
+    return result.status_code
+
+def get_text_path():
+    """Gets file path"""
+    script_dir = os.path.dirname(__file__)
+    rel_path = "websites.txt"
+    return os.path.join(script_dir, rel_path)
+
+def load_websites():
+    """Loads all websites from text file"""
+    with open(str(get_text_path())) as file_contents:
+        content = file_contents.read().splitlines()
+    return content
+
+WEBSITES = load_websites()
+for site in WEBSITES:
+    status_code = https_check(site)
+    if str(status_code) != "200":
+        send_alert("fail")
+
+print("Done")
